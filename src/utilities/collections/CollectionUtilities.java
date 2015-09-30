405348e57d44de07d57a881489fd2c608f4fc58c
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2015-09-30 00:55:50 -0500

    Added map methods and improved style

--- src/utilities/collections/CollectionUtilities.java
+++ src/utilities/collections/CollectionUtilities.java
@@ -1,11 +1,11 @@
-/*
- * A fancy license header.
- */
 package utilities.collections;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * The {@code CollectionUtilities} class contains utility methods related to the
@@ -33,7 +33,7 @@ public final class CollectionUtilities {
      * Don't let anyone instantiate this class.
      */
     private CollectionUtilities() {
-    
+        throw new UnsupportedOperationException("Can't instantiate this class");
     }
 
     /**
@@ -46,11 +46,10 @@ public final class CollectionUtilities {
      */
     public static void requireNonNullCollection(Collection<?> c) {
         Objects.requireNonNull(c, "Invalid null Collection!");
-        c.stream().forEach((element) -> {
-            if (element == null) {
-                throw new NullPointerException("Invald null element in c");
-            }
-        });
+        boolean containsNullObject = c.stream().anyMatch(Objects :: isNull);
+        if (containsNullObject) {
+            throw new NullPointerException("Invald null element in c");
+        }
     }
     
     /**
@@ -82,12 +81,13 @@ public final class CollectionUtilities {
      * @return The trimmed version of the given array.
      */
     public static Object[] trimToSize(Object[] elements, int desiredCapacity) {
+        Object[] trimmed = elements;
         if (elements.length > desiredCapacity) {
-            elements = (desiredCapacity == 0)
+            trimmed = (desiredCapacity == 0)
                     ? EMPTY_OBJECT_ARRAY
                     : Arrays.copyOf(elements, desiredCapacity);
         }
-        return elements;
+        return trimmed;
     }
     
     /**
@@ -127,6 +127,46 @@ public final class CollectionUtilities {
     public static String outOfBoundsMessage(Object[] array, int index) {
         Objects.requireNonNull(array, "Invalid null array!");
         return "Index = " + index + ", Capacity = " + array.length;
+    }
+    
+    /**
+     * Returns a {@code Set} containing all keys that match a given value in the
+     * given map. If there are no keys that match the value, then the returned 
+     * set will be empty.
+     * 
+     * @param <K> The type of key that is stored in the given map and type of 
+     *        object stored in the returned set.
+     * @param <V> The type of value stored in the given map.
+     * @param map The map whose keys to retrieve from the given value.
+     * @param value The value that the desired keys are mapped to.
+     * @return A {@code Set} containing all keys that match a given value in the
+     *         given map.
+     */
+    public static <K, V> Set<K> getKeysByValue(Map<K, V> map, V value) {
+        return map.entrySet().stream()
+                  .filter(entry -> Objects.equals(entry.getValue(), value))
+                  .map(Map.Entry :: getKey)
+                  .collect(Collectors.toSet()); // Terminal operation
+    }
+    
+    /**
+     * Returns the first key in the given map that matches the given value, or 
+     * {@code null} if there is no such key.
+     * 
+     * @param <K> The type of key that is stored in the given map and the type
+     *        of the return value.
+     * @param <V> The type of value stored in the given map.
+     * @param map The map whose key to attempt to match to the given value.
+     * @param value The value that the desired key is mapped to.
+     * @return The first key in the given map that matches the given value, or
+     *         {@code null} if there is no such key.
+     */
+    public static <K, V> K getKeyByValue(Map<K, V> map, V value) {
+        return map.entrySet().stream()
+                  .filter(entry -> Objects.equals(entry.getValue(), value))
+                  .map(Map.Entry :: getKey)
+                  .findFirst() // Terminal operation
+                  .orElse(null);
     }
     
 }
